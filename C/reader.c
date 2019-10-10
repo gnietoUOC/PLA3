6b1ee92e86c25b0baeee23af4cbeefd567404392
Genaro Nieto <gnieto@uoc.edu>
2019-10-10 13:58:12 +0000

    Probando ramas

--- C/reader.c
+++ C/reader.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <iconv.h>
 
 int dump(char *name) {
 	FILE *file;
 	char line[256];
+	char line2[256];
+	iconv_t cd; 
+	size_t size;
+	size_t size2 = 256;
+
+	char *pline = line; 
+	char *pline2 = line2; 
 
 	printf("Leyendo: %s\n\n",name);
 
@@ -13,10 +23,17 @@ int dump(char *name) {
 		return 2;
 	}
 
+	cd = iconv_open("UTF-8","ISO-8859");
+
 	// Leo el fichero línea a a línea
 	while(fgets(line,sizeof(line),file)) {
+		size = strlen(line);
+		iconv(cd,&pline,&size,&pline2,&size2);
+
 		printf("%s",line);
 	}
+
+	iconv_close(cd);
 
 	// Cierro el fichero
 	fclose(file);
